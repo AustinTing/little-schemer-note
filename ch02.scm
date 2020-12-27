947bdfa0ac81cc2e27798d9b7afed4139a772a69
CellBody <austintingwork@gmail.com>
2020-12-27 21:01:03 +0800

    Add ch02.scm

---
+++ ch02.scm
@@ -0,0 +1,20 @@
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+(define lat?
+  (lambda (l)
+    (cond
+     ((null? l) #t)
+     ((atom? (car l)) (lat? (cdr l)))
+     (else #f)))) ; A list can be empty, can have an atom in the first position, or can have a listin the first position.
+
+(define member?
+  (lambda (a lat)
+    (cond
+     ((null ? lat) #f )
+     (else (or ( eq? ( car lat) a)
+	       (member? a ( cdr lat)))))))
+
+; The First Commandment
+; Always ask null? as the first question in expressing any function.
