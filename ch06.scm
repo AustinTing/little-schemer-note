0ba553b6c2d0c29b952d032c3148a05ef2ba083c
CellBody <austintingwork@gmail.com>
2021-01-22 22:36:31 +0800

    Update value.
    Abstract operator and first/sencond sub exp with each new function.

--- ch06.scm
+++ ch06.scm
@@ -102,18 +102,32 @@
 (numbered? '(1 x 1))
 (numbered? '(1 x p))
 
+(define operator
+  (lambda (aexp)
+    (car aexp)))
+
+(define 1st-sub-exp
+  (lambda (aexp)
+    (car (cdr aexp))))
+
+(define 2nd-sub-exp
+  (lambda (aexp)
+    (car (cdr (cdr aexp)))))
+
 (define value
   (lambda (nexp)
     (cond 
      ((atom? nexp) nexp)
-     ((eq? (car nexp) (quote +)) (jia (value (car (cdr nexp)))
-				      (value (car (cdr (cdr nexp))))))
-     ((eq? (car nexp) (quote x)) (cheng (value (car (cdr nexp)))
-					(value (car (cdr (cdr nexp))))))
-     (else (up (value (car (cdr nexp)))
-	       (value (car (cdr (cdr nexp)))))))))
+     ((eq? (operator nexp) (quote +)) (jia (value (1st-sub-exp nexp))
+					   (value (2nd-sub-exp nexp))))
+     ((eq? (operator nexp) (quote x)) (cheng (value (1st-sub-exp nexp))
+					     (value (2nd-sub-exp nexp))))
+     (else (up (value (1st-sub-exp nexp)))
+	       (value (2nd-sub-exp nexp))))))
 
 (up 2 3)
 
 (value '(+ 1 (x 2 3)))
+
+
 
