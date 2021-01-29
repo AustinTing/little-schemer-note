101224252b3d09a10a325fc1cff053b9b0fa1b1b
CellBody <austintingwork@gmail.com>
2021-01-29 22:25:25 +0800

    Update subset?

--- ch07.scm
+++ ch07.scm
@@ -73,3 +73,8 @@
 	    ((member? (car set1) set2) (subset? (cdr set1) set2))
 	    (else #f))))))
 
+(define subset?
+  (lambda (set1 set2)
+    (cond
+     ((null? set1) #t)
+     (else (and (member? (car set1) set2)) (subset? (cdr set1) set2)))))
