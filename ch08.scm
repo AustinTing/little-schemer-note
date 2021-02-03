cfebb4ae49f25479618e14651e0f2def9ec885b4
CellBody <austintingwork@gmail.com>
2021-02-03 17:56:36 +0800

    Update rember-f by currying

--- ch08.scm
+++ ch08.scm
@@ -13,3 +13,13 @@
      ((null? l) '())
      ((test? a (car l)) (cdr l))
      (else (cons (car l) ((rember-f test? a (cdr l))))))))
+
+(define rember-f
+  (lambda (test?)
+    (lambda (a l)
+      (cond
+       ((null? l) '())
+       ((test? a (car l)) (cdr l))
+       (else (cons (car l) ((rember-f test?) a (cdr l))))))))
+
+((rember-f equal?) 'a '(c b a))
