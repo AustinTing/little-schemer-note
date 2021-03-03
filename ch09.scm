2fc2fdfea7a196b123dfe2fd799ab6dd3e5e615a
CellBody <austintingwork@gmail.com>
2021-03-03 18:39:55 +0800

    Add length*

--- ch09.scm
+++ ch09.scm
@@ -20,6 +20,19 @@
 (looking 'caviar '(6 2 4 caviar 5 7 3))
 (looking 'caviar '(6 2 grits caviar 5 7 3))
 
+(define shift
+  (lambda (l)
+    (build (first (first l))
+	   (build (second (first l))
+		  (second l)))))
 
+(shift '((a b) (c d)))
 
-     
+(define length*
+  (lambda (l)
+    (cond
+     ((atom? l) 1)
+     (else (jia (length* (first l))
+		(length* (second l)))))))
+
+(length* '((a b) (c d)))
